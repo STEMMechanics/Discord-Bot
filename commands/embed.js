1ac7d7fd1595f42c5a20ba199e2795c022df8a75
James Collins <james.collins@outlook.com.au>
2023-01-14 21:20:18 +1000

    embeds

---
+++ commands/embed.js
@@ -0,0 +1,41 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('embed')
+		.setDescription('Embeds text')
+		.addStringOption(option =>
+			option.setName('item')
+				.setDescription('The item to embed')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Rules', value: 'rules' },
+				))
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+	async execute(interaction) {
+		const item = interaction.options.getString('item');
+		if(item == 'rules') {
+			const exampleEmbed = new EmbedBuilder()
+			.setColor(0x0099FF)
+			.setTitle('Some title')
+			.setURL('https://discord.js.org/')
+			.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+			.setDescription('Some description here')
+			.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+			.addFields(
+				{ name: 'Regular field title', value: 'Some value here' },
+				{ name: '\u200B', value: '\u200B' },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+			)
+			.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+			.setImage('https://i.imgur.com/AfFp7pu.png')
+			.setTimestamp()
+			.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+		
+			// exampleEmbed
+			return interaction.reply({ embeds: [exampleEmbed] });
+		}
+
+	},
+};
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,5 @@
 const objectutils = require('js-object-utilities'); 
-const { gameChatChannelId } = require('../config.json');
+const { gameChatChannelId, roleRunBotAdminCommands } = require('../config.json');
 const api = require('../utils/api.js');
 const store = require('../utils/store.js');
 
@@ -7,14 +7,18 @@ module.exports = {
     name: 'messageCreate',
     async execute(message) {
         if (!message.author.bot) {
-            if (gameChatChannelId && message.channelId == gameChatChannelId) {
-                api('chat', 'POST', {
-                    'playername': message.author.username,
-                    'message': message.content
-                }).then((response) => {
-                    // console.log(`here, response: ${response.status}, data: ${JSON.stringify(response.json)}`);
-                });
-            }
+            // if (message.author.roles.cache.some(role => role.name === roleRunBotAdminCommands)) {
+            //     if(message.content.starts)
+            // }
+        
+            // if (gameChatChannelId && message.channelId == gameChatChannelId) {
+            //     api('chat', 'POST', {
+            //         'playername': message.author.username,
+            //         'message': message.content
+            //     }).then((response) => {
+            //         // console.log(`here, response: ${response.status}, data: ${JSON.stringify(response.json)}`);
+            //     });
+            // }
 
             // var data = store.load();
             // objects.increment(data, message.author.id + '.messages');

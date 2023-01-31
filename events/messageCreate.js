6704f0a5295e202c8f64ede121350baa4f4e4876
James Collins <james.collins@outlook.com.au>
2023-01-31 21:38:31 +1000

    remove last of Drustcraft

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,5 @@
 const objectutils = require('js-object-utilities'); 
 const { gameChatChannelId, roleRunBotAdminCommands } = require('../config.json');
-const api = require('../utils/api.js');
 const store = require('../utils/store.js');
 
 module.exports = {
@@ -11,15 +10,6 @@ module.exports = {
             //     if(message.content.starts)
             // }
         
-            // if (gameChatChannelId && message.channelId == gameChatChannelId) {
-            //     api('chat', 'POST', {
-            //         'playername': message.author.username,
-            //         'message': message.content
-            //     }).then((response) => {
-            //         // console.log(`here, response: ${response.status}, data: ${JSON.stringify(response.json)}`);
-            //     });
-            // }
-
             // var data = store.load();
             // objects.increment(data, message.author.id + '.messages');
             // store.save(data);

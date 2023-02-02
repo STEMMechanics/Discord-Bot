136bca8d568e1d2d9597900f90a8d215c17f032a
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-02-02 18:01:58 +1000

    fix errors

--- commands/resetGame.js
+++ commands/resetGame.js
@@ -8,16 +8,16 @@ module.exports = {
     .setDescription('Resets the counting game, this operation is not reversible.')
     .addIntegerOption(new SlashCommandIntegerOption()
       .setName('highscore')
-      .setDescription('The highscore reached in this session of the counting game.'))
-    .setRequired(true)
+      .setDescription('The highscore reached in this session of the counting game.')
+	  .setRequired(true))
     .addStringOption(new SlashCommandStringOption()
       .setName('reason')
-      .setDescription('Why did the chain end here?'))
-    .setRequired(true)
-    .addStringOption(new SlashCommandStringOption()
+      .setDescription('Why did the chain end here?')
+	  .setRequired(true))
+	.addStringOption(new SlashCommandStringOption()
       .setName('date')
-      .setDescription("PLEASE PLEASE PLEASE PLEASE FORMAT AS DD/MM/YYYY I DON'T HAVE A WAY TO CHECK"))
-    .setRequired(true),
+      .setDescription("PLEASE PLEASE PLEASE PLEASE FORMAT AS DD/MM/YYYY I DON'T HAVE A WAY TO CHECK")
+	  .setRequired(true)),
   async execute(interaction) {
     gameEmbed(interaction.options.getInteger('highscore'), interaction.options.getString('date'), interaction.options.getString('reason'), interaction);
     store.load();

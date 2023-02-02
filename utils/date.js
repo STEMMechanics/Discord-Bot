45f845d2c6f95e19c442e5f3768746570ab89c51
James Collins <james.collins@outlook.com.au>
2023-02-03 00:07:30 +1000

    add date parser

---
+++ utils/date.js
@@ -0,0 +1,42 @@
+function parseDate(str) {
+  const parts = str.split(/[/-]/);
+
+  if (parts.length === 3) {
+    if (parts[2].length === 2) {
+      parts[2] = `20${parts[2]}`;
+    }
+    if (parts[2].length !== 4 || /^-?\d+$/.test(parts[2]) === false || parseInt(parts[2], 10) < 2020 || parseInt(parts[1], 10) > 2025) {
+      return '';
+    }
+
+    if (parts[1].length === 1) {
+      parts[1] = `0${parts[1]}`;
+    }
+    if (parts[1].length !== 2 || /^-?\d+$/.test(parts[1]) === false || parseInt(parts[1], 10) < 0 || parseInt(parts[1], 10) > 12) {
+      return '';
+    }
+
+    if (parts[0].length === 1) {
+      parts[0] = `0${parts[0]}`;
+    }
+    if (parts[0].length !== 2 || /^-?\d+$/.test(parts[0]) === false) {
+      return '';
+    }
+
+    if (parseInt(parts[0], 10) < 0
+        || ([4, 6, 9, 11].includes(parseInt(parts[1], 10)) && parseInt(parts[0], 10) > 30)
+        || (parseInt(parts[1], 10) === 2 && parseInt(parts[0], 10) > 29)
+        || (parseInt(parts[0], 10) > 31)
+    ) {
+      return '';
+    }
+
+    return `${parts[0]}/${parts[1]}/${parts[2]}`;
+  }
+
+  return '';
+}
+
+module.exports = {
+  parseDate,
+};

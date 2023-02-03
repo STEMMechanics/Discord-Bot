54dc0a1ed9ca2958a4e9cef30e9f98a47303cf6b
James Collins <james.collins@outlook.com.au>
2023-02-03 13:57:01 +1000

    added statuses

---
+++ features/updateStatus.js
@@ -0,0 +1,25 @@
+module.exports = {
+  timers: [{
+    delay: 1800,
+    execute(client) {
+      const activities = [{
+        type: 'PLAYING',
+        text: 'with fire',
+      }, {
+        type: 'PLAYING',
+        text: 'with Stop Motion',
+      }, {
+        type: 'PLAYING',
+        text: 'with Green Screens',
+      }, {
+        type: 'PLAYING',
+        text: 'Minecraft',
+      },
+      ];
+
+      const activity = activities[Math.floor(Math.random() * activities.length)];
+
+      client.user.setActivity(activity.text, { type: activity.type });
+    },
+  }],
+};

23581fc95d119ab475c060545272b6bc5d2ce62f
James Collins <james.collins@outlook.com.au>
2023-02-03 14:35:23 +1000

    update

--- features/updateStatus.js
+++ features/updateStatus.js
@@ -19,7 +19,9 @@ module.exports = {
 
       const activity = activities[Math.floor(Math.random() * activities.length)];
       process.stdout.write(`Name: ${activity.text} Type: ${activity.type}\n`);
-      client.user.setPresence({ activity: { name: activity.text, type: activity.type }, status: 'online' });
+      // Set the client user's presence
+      client.user.setPresence({ activities: [{ name: 'with discord.js' }], status: 'idle' });
+      // client.user.setPresence({ activity: { name: activity.text, type: activity.type }, status: 'online' });
       // client.user.setActivity(activity.text, { type: activity.type });
     },
   }],

816aee1665547defb171c34657e1aae5f540dce8
James Collins <james.collins@outlook.com.au>
2023-02-03 14:48:49 +1000

    cleaup

--- features/updateStatus.js
+++ features/updateStatus.js
@@ -1,6 +1,18 @@
+/*
+  https://discord-api-types.dev/api/discord-api-types-v10/enum/ActivityType#Playing
+
+  Types:
+  Competing - 5
+  Custom - 4 (not available for bots?)
+  Listening - 2
+  Playing - 0
+  Streaming - 1
+  Watching - 3
+*/
+
 module.exports = {
   timers: [{
-    delay: 10,
+    delay: 30,
     execute(client) {
       const activities = [{
         type: 0,
@@ -18,12 +30,7 @@ module.exports = {
       ];
 
       const activity = activities[Math.floor(Math.random() * activities.length)];
-      process.stdout.write(`Name: ${activity.text} Type: ${activity.type}\n`);
-      // Set the client user's presence
       client.user.setPresence({ activities: [{ name: activity.text, type: activity.type }], status: 'online' });
-      // client.user.setPresence({ activity: { name: activity.text, type: activity.type },
-      // status: 'online' });
-      // client.user.setActivity(activity.text, { type: activity.type });
     },
   }],
 };

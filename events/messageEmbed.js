90d007cfe029562e1be106d61dee14d4c5a22615
James Collins <james.collins@outlook.com.au>
2023-11-21 09:58:29 +1000

    catch errors

--- events/messageEmbed.js
+++ events/messageEmbed.js
@@ -8,35 +8,39 @@ module.exports = {
   events: [{
     name: 'messageCreate',
     async execute(message) {
-      process.stderr.write('running handler for message\n');
-      if (!message.content.endsWith('/embed')) {
-        process.stderr.write('messages does not end with embed\n');
-        return;
+      try {
+        process.stderr.write('running handler for message\n');
+        if (!message.content.endsWith('/embed')) {
+          process.stderr.write('messages does not end with embed\n');
+          return;
+        }
+
+        const trimmedContent = message.content.slice(0, -6);
+        const lines = trimmedContent.split('\n');
+        const title = lines.shift();
+        const description = lines;// trimLines(lines);
+        if (description.length === 0) {
+          process.stderr.write('description length is 0\n');
+          return;
+        }
+
+        const embed = new EmbedBuilder()
+          .setColor(0x000000)
+          .setTitle(title)
+          .setDescription(description.join('\n'));
+
+        const announcementChannel = message.client.channels.cache.get(announcementChannelId);
+        if (announcementChannel && announcementChannel.isText()) {
+          announcementChannel.send(embed);
+        } else {
+          process.stderr.write('announcementChannel null\n');
+        }
+
+        message.delete();
+        process.stderr.write('complete\n');
+      } catch (error) {
+        process.stderr.write(`${error}\n`);
       }
-
-      const trimmedContent = message.content.slice(0, -6);
-      const lines = trimmedContent.split('\n');
-      const title = lines.shift();
-      const description = lines;// trimLines(lines);
-      if (description.length === 0) {
-        process.stderr.write('description length is 0\n');
-        return;
-      }
-
-      const embed = new EmbedBuilder()
-        .setColor(0x000000)
-        .setTitle(title)
-        .setDescription(description.join('\n'));
-
-      const announcementChannel = message.client.channels.cache.get(announcementChannelId);
-      if (announcementChannel && announcementChannel.isText()) {
-        announcementChannel.send(embed);
-      } else {
-        process.stderr.write('announcementChannel null\n');
-      }
-
-      message.delete();
-      process.stderr.write('complete\n');
     },
   }],
 };
